c7255c9dda3c8bbdb49c71a8884159a8997d5b24
Tharun Kumar <tharunkumar0120@gmail.com>
2022-05-28 21:53:13 +0530

    right bar and bottom bar

---
+++ components/Card.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
